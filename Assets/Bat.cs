5b89599d97520f6b29ede591e432f3cb382c358e
yishoka <63327438+yishoka@users.noreply.github.com>
2020-05-04 16:53:54 +0800

    Initial commit

---
+++ Assets/Bat.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Bat : MonoBehaviour
+{
+    /// <summary>
+    /// 定義欄位
+    /// </summary>
+    #region 定義基礎數值
+    [Header("蝙蝠的血量"),Tooltip("蝙蝠的血量")]
+    public int BHP;
+    [Header("蝙蝠的攻擊力"),Tooltip("蝙蝠的攻擊力")]
+    public int BATK;
+    [Header("蝙蝠的恢復力"),Tooltip("蝙蝠的恢復力")]
+    public int BHEAL;
+    [Header("蝙蝠輸出結果")]
+    public Text BResult;
+    [Header("對象怪物"),Tooltip("對象怪物")]
+    public Slime Slime1;
+    #endregion
+
+    //遊戲開始時輸入欄位值
+    public void Start()
+    {
+        BHP = 300;
+        BATK = 20;
+        BHEAL = 10;
+    }
+
+    /// <summary>
+    /// 蝙蝠攻擊-呼叫史萊姆受傷方法
+    /// </summary>
+    public void BAttack()
+    {
+        Slime1.SHurt(BATK);
+    }
+
+    /// <summary>
+    /// 蝙蝠受傷-接受史萊姆ATK
+    /// </summary>
+    /// <param name="EnemyATK">敵人的攻擊力</param>
+    public void BHurt(int EnemyATK)
+    {
+        BHP -= EnemyATK;
+        BResult.text = gameObject.name + "收到傷害" + EnemyATK + "\n" + gameObject.name + "剩餘血量：" + BHP; 
+    }
+
+    /// <summary>
+    /// 蝙蝠補血
+    /// </summary>
+    public void BHealling()
+    {
+        BHP += BHEAL;
+        BResult.text = gameObject.name + "恢復血量" + BHEAL + "\n" + gameObject.name + "剩餘血量：" + BHP;
+    }
+}
---
+++ Assets/Slime.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Slime : MonoBehaviour
+{
+    /// <summary>
+    /// 定義欄位
+    /// </summary>
+    #region 定義基礎數值
+    [Header("史萊姆的血量"), Tooltip("史萊姆的血量")]
+    public int SHP;
+    [Header("史萊姆的攻擊力"), Tooltip("史萊姆的攻擊力")]
+    public int SATK;
+    [Header("史萊姆的恢復力"), Tooltip("史萊姆的恢復力")]
+    public int SHEAL;
+    [Header("史萊姆輸出結果")]
+    public Text SResult;
+    [Header("對象怪物"), Tooltip("對象怪物")]
+    public Bat Bat1;
+    #endregion
+
+    //遊戲開始時輸入欄位值
+    public void Start()
+    {
+        SHP = 100;
+        SATK = 10;
+        SHEAL = 30;
+    }
+
+    /// <summary>
+    /// 史萊姆攻擊-呼叫蝙蝠受傷方法
+    /// </summary>
+    public void SAttack()
+    {
+        Bat1.BHurt(SATK);
+    }
+
+    /// <summary>
+    /// 史萊姆受傷-接受BATK
+    /// </summary>
+    /// <param name="EnemyATK">敵人的攻擊力</param>
+    public void SHurt(int EnemyATK)
+    {
+        SHP -= EnemyATK;
+        SResult.text = gameObject.name + "收到傷害" + EnemyATK + "\n" + gameObject.name + "剩餘血量：" + SHP;
+    }
+
+    /// <summary>
+    /// 史萊姆補血
+    /// </summary>
+    public void SHealling()
+    {
+        SHP += SHEAL;
+        SResult.text = gameObject.name + "恢復血量" + SHEAL + "\n" + gameObject.name + "剩餘血量：" + SHP;
+    }
+}
